818f220b2d46107322aa4d23b88751bbcc3b8eb2
napster <napster98766@gmail.com>
2022-10-27 17:34:57 +0530

    day 4

--- lib/login.dart
+++ lib/login.dart
@@ -5,17 +5,48 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-          child: Text(
-        "Login Page",
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
+    return Material(
+      color: Colors.white,
+      child: Column(children: [
+        const SizedBox(height: 20),
+        Image.asset("assets/images/login.png", fit: BoxFit.cover),
+        const Text(
+          "Welcome",
+          style: TextStyle(
+            fontSize: 15,
+            color: Color.fromARGB(255, 127, 20, 248),
+            fontWeight: FontWeight.bold,
+          ),
+          textScaleFactor: 1.75,
         ),
-        textScaleFactor: 1.75,
-      )),
+        const SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Enter Username",
+                  labelText: "Username",
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(),
+          child: const Text("Login"),
+        ),
+      ]),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login.dart';
 import 'package:flutter_application_1/pages/home.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Myapp());
@@ -10,15 +11,16 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         // primarySwatch: Colors.green
       ),
       theme: ThemeData(
+        fontFamily: GoogleFonts.notoSans().fontFamily,
         primarySwatch: Colors.deepPurple,
       ),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         "/": (context) => const Login(),
         "/home": (context) => const Home(),
